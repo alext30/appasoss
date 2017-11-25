147308dc70a2e9bf864fa59c0bebf55c48c81b4b
Alext3o <alexteo222@gmail.com>
2017-11-25 18:34:18 -0500

    base de datos

---
+++ modelo/Database.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * Clase utilitaria que maneja la conexion/desconexion a la base de datos
+ * mediante las funciones PDO (PHP Data Objects).
+ * Utiliza el patron de diseno singleton para el manejo de la conexion.
+ * @author mrea
+ */
+class Database {
+
+    //Propiedades estaticas con la informacion de la conexion (DSN):
+    private static $dbName = 'ddbn3kjciqjl77';
+    private static $dbHost = 'ec2-107-21-248-129.compute-1.amazonaws.com';
+    private static $dbUsername = 'pgiwwetdzvyeoo';
+    private static $dbUserPassword = 'f675480f9a6851bea0fb98a055e79dc112b51176847bec600b596a35af69fa89';
+    //Propiedad para control de la conexion:
+    private static $conexion = null;
+
+    /**
+     * No se permite instanciar esta clase, se utilizan sus elementos
+     * de tipo estatico.
+     */
+    public function __construct() {
+        exit('No se permite instanciar esta clase.');
+    }
+
+    /**
+     * Metodo estatico que crea una conexion a la base de datos.
+     * @return type
+     */
+    public static function connect() {
+        // Una sola conexion para toda la aplicacion (singleton):
+        if (null == self::$conexion) {
+            try {
+                self::$conexion = new PDO("mysql:host=" . self::$dbHost . ";" . "dbname=" . self::$dbName, self::$dbUsername, self::$dbUserPassword);
+            } catch (PDOException $e) {
+                die($e->getMessage());
+            }
+        }
+        return self::$conexion;
+    }
+
+    /**
+     * Metodo estatico para desconexion de la bdd.
+     */
+    public static function disconnect() {
+        self::$conexion = null;
+    }
+
+}
+
+?>
