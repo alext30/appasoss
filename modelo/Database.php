d04ce7619dd3115a2d11628d60c127db7ff96f5d
MR.SoundXperia <reneipiales1200@gmail.com>
2017-11-27 22:16:59 -0500

    Primera conexion establecida

--- modelo/Database.php
+++ modelo/Database.php
@@ -11,6 +11,7 @@ class Database {
     //Propiedades estaticas con la informacion de la conexion (DSN):
     private static $dbName = 'ddbn3kjciqjl77';
     private static $dbHost = 'ec2-107-21-248-129.compute-1.amazonaws.com';
+    private static $port = '5432';
     private static $dbUsername = 'pgiwwetdzvyeoo';
     private static $dbUserPassword = 'f675480f9a6851bea0fb98a055e79dc112b51176847bec600b596a35af69fa89';
     //Propiedad para control de la conexion:
@@ -20,10 +21,9 @@ class Database {
      * No se permite instanciar esta clase, se utilizan sus elementos
      * de tipo estatico.
      */
-    public function __construct() {
+     public function __construct() {
         exit('No se permite instanciar esta clase.');
     }
-
     /**
      * Metodo estatico que crea una conexion a la base de datos.
      * @return type
@@ -32,21 +32,19 @@ class Database {
         // Una sola conexion para toda la aplicacion (singleton):
         if (null == self::$conexion) {
             try {
-                self::$conexion = new PDO("mysql:host=" . self::$dbHost . ";" . "dbname=" . self::$dbName, self::$dbUsername, self::$dbUserPassword);
+                echo "conexion exitosa";
+                self::$conexion = new PDO("pgsql:host=" . self::$dbHost . ";"."port=".self::$port .";". "dbname=" . self::$dbName, self::$dbUsername, self::$dbUserPassword);
             } catch (PDOException $e) {
                 die($e->getMessage());
             }
         }
         return self::$conexion;
     }
-
     /**
      * Metodo estatico para desconexion de la bdd.
      */
     public static function disconnect() {
         self::$conexion = null;
     }
-
 }
-
 ?>
